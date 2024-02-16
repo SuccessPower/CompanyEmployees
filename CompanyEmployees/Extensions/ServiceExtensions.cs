dc298925f5c77b3b2f29fa4a86d3cd3f4af194b0
Success Imamun <successoi@examroom.ai>
2024-02-16 09:51:40 +0100

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CompanyEmployees.Extensions
+{
+		public static class ServiceExtensions
+		{
+				public static void ConfigureCors(this IServiceCollection services) =>
+						services.AddCors(options =>
+						{
+								options.AddPolicy("CorsPolicy", builder =>
+								builder.AllowAnyOrigin()
+								.AllowAnyMethod()
+								.AllowAnyHeader());
+						});
+
+				public static void ConfigureIISIntegration(this IserviceCollection services) =>
+						services.Configure<IISOptions>(options =>
+						{
+
+						});
+
+
+		}
+}
